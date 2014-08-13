122b93ff59cc0dde3a480badbe1fdff1428a8b9e
manishsethi <manish@sethis.in>
2014-08-13 11:55:42 +0530

    Updated parse_log_new with email module

--- parse_log_new.sh
+++ parse_log_new.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
-FILE= /home/minjar05/workspace/test.txt
+#FILE= /home/minjar05/workspace/test.txt
 
 # Checking Avaialbility of file
-if [ ! -f $FILE ]
-then
-        echo "$FILE does not exists. Please specify correct file name"
-        exit 2
-fi
+#if [ ! -f $FILE ]
+#then
+#        echo "$FILE does not exists. Please specify correct file name"
+#        exit 2
+#fi
  
 # Comparing the appropriate limit
+
 limit=10
 #count=`sudo grep -o "valid" $FILE | wc -l`
 
@@ -18,16 +19,11 @@ sh /usr/lib/nagios/plugins/check_log -F /home/minjar05/workspace/parse_log/qfcha
 count=$(sudo cat output.txt)
 
 
-
-
-
-
-#count=sh /usr/lib/nagios/plugins/check_log -F /home/minjar05/workspace/parse_log/qfchat-data-acquisition.log -O /home/minjar05/workspace/parse_log/tmp.log -q "working" | awk -F'(' '{ print $2 }' | awk -F')' '{print $1}'
 echo "Count: $count"
 echo "Limit: $limit"
 if [ $count >  $limit ]
 then
-    echo "Its working" | mail -s "Task Done" manish.sethi@minjar.com
+    echo "You had reached the limit. Email is send to Admin" | ssmtp -s "Test" manishsethi2009@gmail.com  # Sending email
 else
     echo "its okay"
 fi
